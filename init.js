25fecf7ccf11883b35b72282207b5771307b371d
Mulvey <dlmf1ccd@thompsonschools.org>
2019-02-26 09:28:17 -0700

    It works!

--- init.js
+++ init.js
@@ -8,22 +8,29 @@ const cmd = require("node-cmd");
 function init() {
     console.log('Init')
     const file = fs.createWriteStream("master.zip");
-    request('http://github.com/LovelandHighRobotics1977/robotics-web/archive/master.zip').pipe(fs.createWriteStream('master.zip'));
+    request('http://github.com/LovelandHighRobotics1977/robotics-web/archive/master.zip').pipe(fs.createWriteStream('master.zip').on("finish", () => {
+        extract();
+    }))
     function extract() {
         console.log("Extract")
         
-        /*fs.createReadStream('master.zip').pipe(unzip.Extract({
+        fs.createReadStream('master.zip').pipe(unzip.Extract({
             path: 'site'
-        }));*/
+        }).on('close', function () {
+            console.log("Move")
+            cmd.get("rm -rf ./foo.bar && mv -v ./site/robotics-web-master/* ./site/ && rm -rf ./site/robotics-web-master",
+                function (err, data, stderr) {
+                    build()
+                }
+            );
+        }))
     }
 
     function build() {
         console.log('Beginning Build...')
-        cmd.run(`
-            cd site
-            npm install
-            npm run build
-        `)
+        cmd.get("cd site && npm install && npm run build", () => {
+            console.log("Build Done")
+        })
     }
 }
 
